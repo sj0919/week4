89e56f4f82d96eaf2835eb74485fdcba525e5286
sj0919 <seungjinlee09@gmail.com>
2025-01-17 23:35:08 +0900

    UI

---
+++ src/pages/game/game2/game2_intro.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Game2Intro = () => {
+  return <Layout></Layout>;
+};
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
